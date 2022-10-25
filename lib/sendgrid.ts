c7ca3c80bc262fbaffc4b7b1a0e067affb381275
Walter <daniwortiz003@gmail.com>
2022-10-24 22:18:20 -0300

    remove sendgrid token

--- lib/sendgrid.ts
+++ lib/sendgrid.ts
@@ -1,7 +1,5 @@
 import * as sgMail from "@sendgrid/mail";
-const API_KEY =
-    (process.env.SENDGRID_APIKEY as string) ||
-    "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";
+const API_KEY = process.env.SENDGRID_APIKEY as string;
 sgMail.setApiKey(API_KEY);
 
 async function sendgridMail(msg: sgMail.MailDataRequired) {
