7770994cf41870715029aa8080d5e605ef75783b
Walter2k22 <daniwortiz003@gmail.com>
2023-01-27 11:29:58 -0300

    bug pagination resolved

--- components/products-results/index.tsx
+++ components/products-results/index.tsx
@@ -9,103 +9,100 @@ import { ProductNotFound } from "./not-found";
 import { Pagination } from "./pagination";
 import { SortByModal } from "./sort-by";
 import {
-    Arrow,
-    ArrowDown,
-    Container,
-    ContainerMain,
-    ContainerProducts,
-    Hr,
-    SortBy,
-    SortByContainer,
-    SortByContainerText,
+  Arrow,
+  ArrowDown,
+  Container,
+  ContainerMain,
+  ContainerProducts,
+  Hr,
+  SortBy,
+  SortByContainer,
+  SortByContainerText,
 } from "./styled";
 
 interface Props {
-    query: string;
+  query: string;
 }
 
 function ProductsResults({ query }: Props) {
-    const [offset, setOffset] = useState(0);
-    const [lastQuery, setLastQuery] = useState(query);
-    const [pages, setPages] = useState([] as any);
-    const [modal, setModal] = useState(false);
-    const [productsOrdered, setProductsOrdered] = useState(null as any);
-    const productsData = useGetProductBySearch(query, offset);
-    const products = productsOrdered || productsData;
-    const router = useRouter();
-    
-    useEffect(() => {
-        var offsetAux = 0;
-        var pagination = 1;
-        var pageColl = [] as any;
-        var total = products?.pagination?.total;
-        while (offsetAux < total) {
-            offsetAux += 12;
+  const [offset, setOffset] = useState(0);
+  const [lastQuery, setLastQuery] = useState(query);
+  const [pages, setPages] = useState([] as any);
+  const [modal, setModal] = useState(false);
+  const [productsOrdered, setProductsOrdered] = useState(null as any);
+  const productsData = useGetProductBySearch(query, offset);
+  const products = productsOrdered || productsData;
+  const router = useRouter();
 
-            pageColl.push(pagination++);
-        }
-        setProductsOrdered(null);
-        
-        if ((lastQuery !== query && pageColl.length) || !pages.length ) {
-            setLastQuery(query);
-            setPages(pageColl);
-        } else if(!productsData?.results?.length) setPages(pageColl);
+  useEffect(() => {
+    var offsetAux = 0;
+    var pagination = 1;
+    var pageColl = [] as any;
+    var total = products?.pagination?.total;
+    while (offsetAux < total) {
+      offsetAux += 12;
 
-        
-    }, [productsData]);
+      pageColl.push(pagination++);
+    }
+    setProductsOrdered(null);
 
-    return (
-        <ContainerMain>
-            {!products ? (
-                <Loader />
-            ) : !products.results.length ? (
-                <ProductNotFound />
-            ) : (
-                <Container>
-                    <SortByContainer>
-                        <SortBy onClick={() => setModal(!modal)}>
-                            <SortByContainerText>
-                                <BodyText>Ordenar Por</BodyText>
-                                {modal ? <Arrow /> : <ArrowDown />}
-                            </SortByContainerText>
-                            <Hr />
-                        </SortBy>
-                        {modal ? (
-                            <SortByModal
-                                onClick={() => setModal(!modal)}
-                                products={productsData?.results}
-                                setProductsOrdered={setProductsOrdered}
-                            />
-                        ) : null}
-                    </SortByContainer>
-                    <ContainerProducts>
-                        {products?.results?.map((i: any) => (
-                            <CardProduct
-                                key={i.objectID}
-                                onClickBuy={() => {
-                                    addToCart(i);
-                                    router.push("/cart");
-                                }}
-                                onClickImg={() =>
-                                    router.push({
-                                        pathname: "/product/",
-                                        query: { id: i.objectID },
-                                    })
-                                }
-                                onClickCart={() => addToCart(i)}
-                                name={i.results.Name}
-                                price={i.results["Unit cost"]}
-                                img={i.results.Images[0].thumbnails.full.url}
-                            />
-                        ))}
-                    </ContainerProducts>
-                </Container>
-            )}
-            {pages.length ? (
-                <Pagination offset={offset} setOffset={setOffset} pages={pages} />
+    if ((lastQuery !== query && pageColl.length) || !pages.length) {
+      setLastQuery(query);
+      setPages(pageColl);
+      setOffset(0);
+    } else if (!productsData?.results?.length) setPages(pageColl);
+  }, [productsData]);
+
+  return (
+    <ContainerMain>
+      {!products ? (
+        <Loader />
+      ) : !products.results.length ? (
+        <ProductNotFound />
+      ) : (
+        <Container>
+          <SortByContainer>
+            <SortBy onClick={() => setModal(!modal)}>
+              <SortByContainerText>
+                <BodyText>Ordenar Por</BodyText>
+                {modal ? <Arrow /> : <ArrowDown />}
+              </SortByContainerText>
+              <Hr />
+            </SortBy>
+            {modal ? (
+              <SortByModal
+                onClick={() => setModal(!modal)}
+                products={productsData?.results}
+                setProductsOrdered={setProductsOrdered}
+              />
             ) : null}
-        </ContainerMain>
-    );
+          </SortByContainer>
+          <ContainerProducts>
+            {products?.results?.map((i: any) => (
+              <CardProduct
+                key={i.objectID}
+                onClickBuy={() => {
+                  addToCart(i);
+                  router.push("/cart");
+                }}
+                onClickImg={() =>
+                  router.push({
+                    pathname: "/product/",
+                    query: { id: i.objectID },
+                  })
+                }
+                onClickCart={() => addToCart(i)}
+                name={i.results.Name}
+                price={i.results["Unit cost"]}
+                img={i.results.Images[0].thumbnails.full.url}
+              />
+            ))}
+          </ContainerProducts>
+        </Container>
+      )}
+      {pages.length ? <Pagination offset={offset} setOffset={setOffset} pages={pages} /> : null}
+    </ContainerMain>
+  );
 }
 
 export { ProductsResults };
--- components/products-results/pagination.tsx
+++ components/products-results/pagination.tsx
@@ -3,62 +3,68 @@ import { BodyText } from "ui/typography";
 import { ContainerNextPage, ArrowLeft, NextPage, ArrowRight } from "./styled";
 
 interface Props {
-    setOffset: Dispatch<SetStateAction<number>>;
-    offset: number;
-    pages: [number];
+  setOffset: Dispatch<SetStateAction<number>>;
+  offset: number;
+  pages: [number];
 }
 
 function Pagination({ setOffset, offset, pages }: Props) {
-    const itemsRef = useRef([] as any);
-    const [backColor, setBackColor] = useState([] as any);
-    const [actualPage, setActualPage] = useState(1);
+  const itemsRef = useRef([] as any);
+  const [backColor, setBackColor] = useState([] as any);
+  const [actualPage, setActualPage] = useState(1);
 
-    useEffect(() => {
-        setBackColor([
-            "",
-            ...pages?.map((i: number) => (i === 1 ? "var(--secondary-color)" : "white")),
-        ]);
-    }, [pages]);
+  useEffect(() => {
+    if (actualPage === 1) return;
+    if (offset !== 0) return;
+    setActualPage(1);
+  }, [offset]);
 
-    function onClickPage(pageN: number) {
-        window.scrollTo(0, 0);
-        if (pageN < actualPage && pageN !== actualPage) setOffset(offset > 0 ? offset - 12 : 0);
-        else if (pageN > actualPage && pageN !== actualPage) setOffset(offset + 12);
+  useEffect(() => {
+    setBackColor([
+      "",
+      ...pages?.map((i: number) => (i === 1 ? "var(--secondary-color)" : "white")),
+    ]);
+  }, [pages]);
 
-        if (pageN !== actualPage) {
-            setActualPage(pageN);
-            const newColors = backColor.map((background: any, index: any) =>
-                index === actualPage ? "white" : background
-            );
-            newColors[pageN] = "var(--secondary-color)";
-            setBackColor(newColors);
-        }
+  function onClickPage(pageN: number) {
+    window.scrollTo(0, 0);
+    if (pageN < actualPage && pageN !== actualPage) setOffset(offset > 0 ? offset - 12 : 0);
+    else if (pageN > actualPage && pageN !== actualPage) setOffset(offset + 12);
+
+    if (pageN !== actualPage) {
+      setActualPage(pageN);
+      const newColors = backColor.map((background: any, index: any) =>
+        index === actualPage ? "white" : background
+      );
+      newColors[pageN] = "var(--secondary-color)";
+      setBackColor(newColors);
     }
+  }
 
-    function onPrevPage() {
-        if (actualPage !== 1) onClickPage(actualPage - 1);
-    }
+  function onPrevPage() {
+    if (actualPage !== 1) onClickPage(actualPage - 1);
+  }
 
-    function onNextPage() {
-        if (actualPage !== pages?.length) onClickPage(actualPage + 1);
-    }
+  function onNextPage() {
+    if (actualPage !== pages?.length) onClickPage(actualPage + 1);
+  }
 
-    return (
-        <ContainerNextPage>
-            <ArrowLeft onClick={onPrevPage} />
-            {pages?.map((n: any) => (
-                <NextPage
-                    onClick={() => onClickPage(n)}
-                    theme={backColor[n]}
-                    key={n}
-                    ref={(el: any) => (itemsRef.current[n] = el)}
-                >
-                    <BodyText>{n}</BodyText>
-                </NextPage>
-            ))}
-            <ArrowRight onClick={onNextPage} />
-        </ContainerNextPage>
-    );
+  return (
+    <ContainerNextPage>
+      <ArrowLeft onClick={onPrevPage} />
+      {pages?.map((n: any) => (
+        <NextPage
+          onClick={() => onClickPage(n)}
+          theme={backColor[n]}
+          key={n}
+          ref={(el: any) => (itemsRef.current[n] = el)}
+        >
+          <BodyText>{n}</BodyText>
+        </NextPage>
+      ))}
+      <ArrowRight onClick={onNextPage} />
+    </ContainerNextPage>
+  );
 }
 
 export { Pagination };
